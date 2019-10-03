c3e4f33cc69f209f967cc399aa06304894183a94
Grace Yang <yaycake@gmail.com>
2019-10-03 17:21:14 +0800

    added loading spinner

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -5,6 +5,8 @@ import IngredientList from './IngredientList';
 
 const Ingredients = () => {
   const [ userIngredients, setUserIngredients ] = useState([])
+  const [ isLoading, setIsLoading] = useState(false)
+
 
   //useEffect runs after every render cycle
   useEffect(()=> {
@@ -32,11 +34,15 @@ const Ingredients = () => {
   }, [])
 
   const addIngredientHandler = ingredient => {
+
+    setIsLoading(true)
+
     fetch('https://react-hooks-update-438f7.firebaseio.com/ingredients.json', {
       method: 'POST', 
       body: JSON.stringify(ingredient), 
       headers: { 'Content-Type': 'application/json'}
     }).then(response => {
+      setIsLoading(false)
       return response.json();
     }).then(responseData => {
       setUserIngredients(prevIngredients => [...prevIngredients, { id: responseData.name, ...ingredient }])
@@ -44,10 +50,12 @@ const Ingredients = () => {
   };
 
   const removeIngredientHandler = ingredientId => {
+    setIsLoading(true)
     fetch(`https://react-hooks-update-438f7.firebaseio.com/ingredients/${ingredientId}.json`, {
       method: 'DELETE'
     })
     .then(response => {
+      setIsLoading(false)
       setUserIngredients(prevIngredients => 
         prevIngredients.filter(ingredient => ingredient.id !== ingredientId)
       )
@@ -56,7 +64,7 @@ const Ingredients = () => {
 
   return (
     <div className="App">
-      <IngredientForm onAddIngredient={addIngredientHandler}/>
+      <IngredientForm onAddIngredient={addIngredientHandler} loading={isLoading}/>
 
       <section>
         <Search onLoadIngredients = {filteredIngredientsHandler}/>
